655e797136113bd4c43ec6a79ecf977ac5614b07
Sohail Mahmud <sohailmahmud@yahoo.com>
2020-07-16 21:27:31 +0600

    Create DailyAttendance.java

---
+++ src/com/Attendance/DailyAttendance.java
@@ -0,0 +1,31 @@
+package com.Attendance;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class DailyAttendance extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	public DailyAttendance()
+	{
+		init();
+		cmp();
+		setBackground(Color.BLUE);
+	}
+
+	private void cmp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void init() 
+	{
+		setPreferredSize(new Dimension(1105, 610));
+		BorderLayout border = new BorderLayout();
+		border.setHgap(10);
+		setLayout(border);
+	}
+}
